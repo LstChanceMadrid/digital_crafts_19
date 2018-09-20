0f8dddd23f579ee95033a70c553d026e5137ecd9
LstChanceMadrid <michael.madrid@live.com>
2018-09-20 08:40:44 -0500

    added some notes

--- factorial.py
+++ factorial.py
@@ -7,10 +7,13 @@ def calculate(input_number):
     else:
         print("Your number must be a positive integer above 0")
 
+
+#factors through all of the numbers starting at the input number
 def factorial(input_number):
     number = input_number + 1
     factorial = 1
     while number > 1:
+        #the method of going through each number
         number -= 1
         factorial *= number
     print(factorial)
--- palindrome.py
+++ palindrome.py
@@ -2,8 +2,10 @@ user_input = input("Enter a word: ")
 
 
 def backwards_user_input(user_input):
+    #defining the variable to store the concatenated letters
     reversed_word = ""
     for index in range(len(user_input) - 1, -1, -1):
+        #adding the letters to the variable beginning from the last index
         reversed_word = reversed_word + user_input[index]
     return reversed_word
 
