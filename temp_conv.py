d960b8d68b77b448b25e4f64e5d592841be9b5b2
LstChanceMadrid <michael.madrid@live.com>
2018-09-20 09:07:20 -0500

    added notes to converter

--- temp_conv.py
+++ temp_conv.py
@@ -2,6 +2,7 @@ user_conversion = input("Enter 'F' to convert to Fahrenheit or 'C' to convert to
 
 user_temperature = int(input("Enter your temperature: "))
 
+#determines the conversion type and relays the temperature
 def conversion_type(conversion, temp):
     if (conversion == 'f'):
         celcius_to_fahrenheit(temp)
@@ -10,11 +11,12 @@ def conversion_type(conversion, temp):
     else:
         print("Invalid option")
 
-
+#converts the given temperature to Celsius
 def fahrenheit_to_celcius(temp):
     celcius = (temp - 32) * 5 / 9
     print(celcius)
 
+#converts the given temperature to fahrenheit
 def celcius_to_fahrenheit(temp):
     fahrenheit = temp * 9 / 5 + 32
     print(fahrenheit)
